4f71395867e8d3352e39c20d7e655854c42ef133
Majdi Ben Youssef <benyoussef.mohamed@esprit.tn>
2024-05-01 01:18:27 +0100

    first commit

---
+++ src/main/java/com/example/projetdevdynamo/ProjetDevDynamoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetdevdynamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDevDynamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDevDynamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetdevdynamo/ProjetDevDynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetdevdynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDevDynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
